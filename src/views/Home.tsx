755884aedbb0da2a0e2b87dc5e22629f6d1d3f43
Roy Palomino Rojas <roy100995@gmail.com>
2024-06-14 14:22:32 -0500

    display

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import Container from "../components/Container";
 import Hero from "../components/Hero";
 import Separator from "../components/Separator";
+import Footer from "../components/Footer";
 import JoinForm from "../components/JoinForm";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <JoinForm />
         </Container>
       </main>
+      <Footer />
     </>
   );
 }
