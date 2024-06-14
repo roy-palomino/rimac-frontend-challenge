8594c36f0c5c9c2525498b343ce493b435bfd553
Roy Palomino Rojas <roy100995@gmail.com>
2024-06-14 13:58:22 -0500

    feat: add config for BS sonoma font as default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      extend: {
+        fontFamily: {
+          sans: ["BRSonoma", "ui-sans-serif", "system-ui"],
+        },
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
