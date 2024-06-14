8499000a9e2af573c0e5e29c221c40fbf8ef461f
Roy Palomino Rojas <roy100995@gmail.com>
2024-06-14 14:05:23 -0500

    refactor: add padding to h1

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,22 @@
 import { FC } from "react";
 
-import Container from "../components/Container";
-
 const Hero: FC = () => {
   return (
-    <section className="">
-      <Container>
-        Something
-      </Container>
+    <section className="grid grid-cols-10 gap-3 h-40">
+      <div className="col-span-6 self-center">
+        <h1 className="bg-gradient-to-r from-[#00F4E2] to-[#00FF7F] text-[12px] font-bold w-[151px] h-5 text-center rounded mb-2 pt-[2px] px-2">
+          Seguro Salud Flexible
+        </h1>
+        <p className="text-[28px] font-bold leading-9">
+          Creado para <br />
+          ti y tu familia
+        </p>
+      </div>
+      <img
+        className="col-span-4 transform -scale-x-100 w-full h-full object-cover rounded-2xl"
+        src="/images/hero-image.png"
+        alt=""
+      />
     </section>
   );
 };
