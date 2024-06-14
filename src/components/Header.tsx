6e8b966d0b29bb8b0957d23a70b20232c21dc8cc
Roy Palomino Rojas <roy100995@gmail.com>
2024-06-14 14:01:00 -0500

    feat: display icon  phone

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,21 @@
 import { FC } from "react";
 
-interface Props {
-};
+import logo from "../assets/logo.svg";
+import { PhoneIcon } from "@heroicons/react/24/solid";
+
+interface Props {}
 
 const Header: FC<Props> = () => {
   return (
-    <nav className="flex flex-row justify-between w-full h-[56px] bg-blue-50 px-6 py-3 items-center">
-      <div>Rimac logo</div>
-      <div>(01) 411 6001</div>
+    <nav className="flex flex-row justify-between w-full h-[56px] px-6 py-3 items-center">
+      <img src={logo}></img>
+      <div className="flex flex-row items-center">
+        <PhoneIcon className="size-4" />
+
+        <span className="ml-2 font-bold text-[16px] leading-5">
+          (01) 411 6001
+        </span>
+      </div>
     </nav>
   );
 };
