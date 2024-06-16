a922ba28401d3e0f361e6e38ce1e4920053550e5
Roy Palomino Rojas <roy100995@gmail.com>
2024-06-15 23:14:19 -0500

    feat: list card options

---
+++ src/components/QuoteOptions.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+import { RadioGroup } from "@headlessui/react";
+
+import QuoteCard from "./QuoteCard";
+
+const plans = [
+  {
+    title: "Para mí",
+    description:
+      "Cotiza tu seguro de salud y agrega familiares si así lo deseas.",
+    icon: "icon1",
+  },
+  {
+    title: "Para alguien más",
+    description:
+      "Realiza una cotización para uno de tus familiares o cualquier persona.",
+    icon: "icon2",
+  },
+];
+
+export default function Example() {
+  const [selected, setSelected] = useState(plans[0]);
+
+  return (
+    <div className="w-full px-4">
+      <div className="mx-auto w-full max-w-md">
+        <RadioGroup
+          // @ts-ignore
+          by="title"
+          value={selected}
+          onChange={setSelected}
+          className="space-y-6"
+        >
+          {plans.map((plan) => (
+            <QuoteCard plan={plan} />
+          ))}
+        </RadioGroup>
+      </div>
+    </div>
+  );
+}
