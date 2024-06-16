edd59ab40644a47f0b9f3f2635ed30a57926c66c
Roy Palomino Rojas <roy100995@gmail.com>
2024-06-15 23:14:45 -0500

    prettier changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,14 +4,16 @@ import logoWhite from "../assets/logo-white.svg";
 
 import Separator from "./Separator";
 
-interface Props {
-};
+interface Props {}
 
 const Footer: FC<Props> = () => {
   return (
-    <footer className="flex flex-col bg-[#03050F] py-8 px-6">
-      <img src={logoWhite}></img>
-      <Separator />
+    <footer className="flex flex-col bg-[#03050F] py-8 items-center">
+      <img className="w-[138px] h-5" src={logoWhite}></img>
+      <Separator className="bg-[#2B304E]" />
+      <p className="text-white text-xs tracking-[0.1px]">
+        &copy; 2023 RIMAC Seguros y Reaseguros.
+      </p>
     </footer>
   );
 };
