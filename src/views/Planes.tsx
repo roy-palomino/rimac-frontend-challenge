72a488ebbfe281ee859ed26ede533227bb245502
Roy Palomino Rojas <roy100995@gmail.com>
2024-06-15 23:57:44 -0500

    refactor: style text and display cards and step counter

--- src/views/Planes.tsx
+++ src/views/Planes.tsx
@@ -1,11 +1,33 @@
 import { FC } from "react";
 
-interface Props {
-};
+import Header from "../components/Header";
+import QuoteOptions from "../components/QuoteOptions";
+import Steps from "../components/StepsComponent";
+
+interface Props {}
 
 const Planes: FC<Props> = () => {
   return (
-    <div>Planes here</div>
+    <>
+      <Header />
+      <main>
+        <Steps />
+
+        <div className="px-6 py-8">
+          <div className="text-[#141938] pb-8">
+            <h2 className="font-bold text-[28px] leading-[36px] -tracking-[0.2px]">
+              Rocío ¿Para quién deseas cotizar?
+            </h2>
+            <p className="leading-7 -tracking-[0.1px] mt-2">
+              Selecciona la opción que se ajuste más a tus necesidades.
+            </p>
+          </div>
+          <div>
+            <QuoteOptions />
+          </div>
+        </div>
+      </main>
+    </>
   );
 };
 
