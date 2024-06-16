61b5efc17d43cceea40bb002ffdd68c30056c1db
Roy Palomino Rojas <roy100995@gmail.com>
2024-06-15 23:13:52 -0500

    feat: add custom shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ export default {
           sans: ["BRSonoma", "ui-sans-serif", "system-ui"],
         },
       },
+      boxShadow: {
+        'rimac-shadow': '0px 1px 32px 0px rgba(174, 172, 243, 0.35)',
+      },
     },
   },
   plugins: [require("@tailwindcss/forms")],
