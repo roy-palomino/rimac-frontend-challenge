e19eb707094653a97eb47130a8744061a67b30c3
Roy Palomino Rojas <roy100995@gmail.com>
2024-06-16 20:13:28 -0500

    feat: add util to get user age

---
+++ src/utils/getUserAge.ts
@@ -0,0 +1,10 @@
+export function getUserAge(birthdate: string) {
+  const today = new Date();
+  const birthDate = new Date(birthdate);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const month = today.getMonth() - birthDate.getMonth();
+  if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
