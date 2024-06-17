cdd4edc745f7ff39dff29c85513d99338b6fbb22
Roy Palomino Rojas <roy100995@gmail.com>
2024-06-16 20:53:06 -0500

    feat: change display and styles for md devices

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,12 +8,14 @@ interface Props {}
 
 const Footer: FC<Props> = () => {
   return (
-    <footer className="flex flex-col bg-[#03050F] py-8 items-center">
-      <img className="w-[138px] h-5" src={logoWhite}></img>
-      <Separator className="bg-[#2B304E]" />
-      <p className="text-white text-xs tracking-[0.1px]">
-        &copy; 2023 RIMAC Seguros y Reaseguros.
-      </p>
+    <footer className="bg-[#03050F] py-8">
+      <div className="flex flex-col  items-center md:flex-row md:justify-between max-w-[1180px] mx-auto">
+        <img className="w-[138px] h-5" src={logoWhite}></img>
+        <Separator className="bg-[#2B304E]" />
+        <p className="text-white text-xs tracking-[0.1px]">
+          &copy; 2023 RIMAC Seguros y Reaseguros.
+        </p>
+      </div>
     </footer>
   );
 };
