fb604e099155eb0b890cdcce3f5472e105ddf11f
Roy Palomino Rojas <roy100995@gmail.com>
2024-06-16 20:53:25 -0500

    refactor: update header for md devices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header: FC = () => {
   const navigate = useNavigate();
 
   return (
-    <nav className="flex flex-row justify-between w-full h-[56px] px-6 py-3 items-center max-w-[1180px] md:mx-auto">
+    <nav className="flex flex-row justify-between w-full h-[56px] px-6 py-3 items-center bg-[#F8F9FF] max-w-[1180px] md:mx-auto">
       <img onClick={() => navigate("/")} src={logo}></img>
       <div className="flex flex-row items-center">
         <p className="hidden md:block font-semibold text-xs text-[#03050F]">¡Compra por este medio!</p>
