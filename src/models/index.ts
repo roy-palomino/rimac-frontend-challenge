4d5c3d9c0d721672a1a6c641a59bc8a47cef7c77
Roy Palomino Rojas <roy100995@gmail.com>
2024-08-19 23:22:40 -0500

    feat: add models to save and import types from there

---
+++ src/models/index.ts
@@ -0,0 +1,12 @@
+export interface Plan {
+  name: string;
+  price: number;
+  description: string[];
+  age: number;
+}
+
+export interface User {
+  name: string;
+  lastName: string;
+  birthDay: string;
+}
